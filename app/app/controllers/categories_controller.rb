f3ec6b0c694a2adef44f87515a00738889004f1f
chechetov <chechetov@github.com>
2022-02-14 21:18:04 +0200

    category_add method

--- app/app/controllers/categories_controller.rb
+++ app/app/controllers/categories_controller.rb
@@ -1,8 +1,19 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category Created"
+
+      redirect_to categories_path
+      
+      else
+        render 'new'
+      end
   end
 
   def edit
@@ -16,4 +27,9 @@ class CategoriesController < ApplicationController
 
   def show
   end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
